242b3c8314303f104d9b36153816cccd7db57a19
Mr. Coxall <mr.coxall@mths.ca>
2022-02-24 19:21:43 +0000

    added math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sept 2020
+# This is the math program
+
+
+def main():
+    print(9 + 2)
+    print("7 - 3 = {}".format(7 - 3))
+    print("2/3 = {}".format(2 / 3))
+    print("4+4/2 = {}".format(4 + 4 / 2))
+    print("3+4*2 = {}".format(3 + 4 * 2))
+    print("5+2^3={} cm²".format(5 + pow(2, 3)))
+
+
+if __name__ == "__main__":
+    main()
