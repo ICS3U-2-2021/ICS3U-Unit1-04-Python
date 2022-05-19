5f2380d5c8fbd60daa243fecbb18123979a298a4
ICS3U.2.2021 <ics3u.2.2021@mths.ca>
2022-05-19 12:28:34 +0000

    update

--- math.py
+++ math.py
@@ -6,12 +6,14 @@
 
 
 def main():
+    print("\nStart ...\n")
     print(9 + 2)
     print("7 - 3 = {}".format(7 - 3))
     print("2/3 = {}".format(2 / 3))
     print("4+4/2 = {}".format(4 + 4 / 2))
     print("3+4*2 = {}".format(3 + 4 * 2))
     print("5+2^3={} cm²".format(5 + pow(2, 3)))
+    print("\nDone.")
 
 
 if __name__ == "__main__":
